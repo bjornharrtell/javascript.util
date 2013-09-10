3370b4084264ba1671d4e22dc19a83ec6172de12
Björn Harrtell <bjorn@wololo.org>
2013-09-10 22:31:28 +0200

    Expose full API

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 webmake ../lib/browser.js javascript.util.js
+java -jar compiler.jar --js javascript.util.js --js_output_file javascript.util.min.js
+cat ../license-notice.txt javascript.util.min.js > javascript.util-0.11.0.min.js
