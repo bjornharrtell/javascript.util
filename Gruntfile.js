f648e31b4e858e7c32bb1b605d94d7b1f4c346c0
Björn Harrtell <bjorn@wololo.org>
2014-10-23 21:47:48 +0200

    Add build script Cleanups

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,25 @@
 module.exports = function(grunt) {
-
-
     grunt.initConfig({
-
         pkg: grunt.file.readJSON('package.json'),
-        
+
         bower: {
-    install: {
-    }
-  },
-        
+            install: {}
+        },
+
         gjslint: {
             options: {
-              flags: [
-                '--disable 110' //ignore error code 220 from gjslint
-              ],
-              reporter: {
-                name: 'console'
-              }
+                flags: [
+                    '--disable 110'
+                ],
+                reporter: {
+                    name: 'console'
+                }
             },
             all: {
-              src: ['src/**/*.js']
+                src: ['src/**/*.js']
             }
-          },
-        
-        
+        },
+
         closureCompiler: {
             options: {
                 compilerFile: 'lib/closure-compiler/compiler.jar',
@@ -68,7 +63,8 @@ module.exports = function(grunt) {
                     '!lib/closure-library/closure/goog/demos/**/*.js',
                     'lib/closure-library/third_party/closure/goog/**/*.js',
                     '!lib/closure-library/third_party/closure/goog/**/*_test.js',
-                    '!lib/closure-library/third_party/closure/goog/**/*_perf.js'],
+                    '!lib/closure-library/third_party/closure/goog/**/*_perf.js'
+                ],
                 dest: 'dist/javascript.util.js'
             },
             node: {
@@ -83,59 +79,12 @@ module.exports = function(grunt) {
                     'lib/closure-library/third_party/closure/goog/**/*.js',
                     '!lib/closure-library/third_party/closure/goog/**/*_test.js',
                     '!lib/closure-library/third_party/closure/goog/**/*_perf.js',
-                    'src/node/*.js'],
+                    'src/node/*.js'
+                ],
                 dest: 'dist/javascript.util-nodejs.js'
             }
         },
-        
-        closureCompilerNode: {
-            options: {
-                compilerFile: 'lib/closure-compiler/compiler.jar',
-                compilerOpts: {
-                    compilation_level: 'ADVANCED',
-                    externs: ['lib/closurecompiler-externs/*.js'],
-                    generate_exports: true,
-                    define: ["'goog.DEBUG=false'"],
-                    warning_level: 'verbose',
-                    summary_detail_level: 3,
-                    manage_closure_dependencies: true,
-                    closure_entry_point: [
-                        'javascript.util.ArrayList',
-                        'javascript.util.Arrays',
-                        'javascript.util.Collection',
-                        'javascript.util.EmptyStackException',
-                        'javascript.util.HashMap',
-                        'javascript.util.HashSet',
-                        'javascript.util.IndexOutOfBoundsException',
-                        'javascript.util.Iterator',
-                        'javascript.util.List',
-                        'javascript.util.Map',
-                        'javascript.util.NoSuchElementException',
-                        'javascript.util.OperationNotSupported',
-                        'javascript.util.Set',
-                        'javascript.util.SortedMap',
-                        'javascript.util.SortedSet',
-                        'javascript.util.Stack',
-                        'javascript.util.TreeMap',
-                        'javascript.util.TreeSet'
-                    ],
-                    output_wrapper: '"(function(){%output%}).call(this);"'
-                }
-            },
-            main: {
-                src: ['src/*.js',
-                    'lib/closure-library/closure/goog/**/*.js',
-                    '!lib/closure-library/closure/goog/**/*_test.js',
-                    '!lib/closure-library/closure/goog/**/*_perf.js',
-                    '!lib/closure-library/closure/goog/demos/**/*.js',
-                    'lib/closure-library/third_party/closure/goog/**/*.js',
-                    '!lib/closure-library/third_party/closure/goog/**/*_test.js',
-                    '!lib/closure-library/third_party/closure/goog/**/*_perf.js',
-                    'src/node/*.js'],
-                dest: 'dist/javascript.util-nodejs.js'
-            }
-        },
-        
+
         closureDepsWriter: {
             main: {
                 options: {
@@ -144,13 +93,12 @@ module.exports = function(grunt) {
                 },
                 dest: 'deps.js'
             }
-        },
-    
+        }
     });
-    
+
     grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-closure-tools');
     grunt.loadNpmTasks('grunt-gjslint');
-    
+
     grunt.registerTask('default', ['bower', 'gjslint', 'closureDepsWriter', 'closureCompiler']);
 }
