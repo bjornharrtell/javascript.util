12d042f572caf4f3a3e37a823371fcbb3c7141fa
Björn Harrtell <bjorn@wololo.org>
2014-10-23 22:00:20 +0200

    Remove old build stuff

--- build/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-webmake ../lib/browser.js javascript.util.js
-java -jar compiler.jar --js javascript.util.js --js_output_file javascript.util.min.js
-cat ../license-notice.txt javascript.util.min.js > javascript.util-0.11.0.min.js
