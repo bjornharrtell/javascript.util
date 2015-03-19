cb5351a838f2339afcaa5f62f9ce51945dd328a2
Björn Harrtell <bjorn@wololo.org>
2015-03-19 23:02:06 +0100

    Simplified build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,10 +2,6 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        bower: {
-            install: {}
-        },
-
         gjslint: {
             options: {
                 flags: [
@@ -22,36 +18,9 @@ module.exports = function(grunt) {
 
         closureCompiler: {
             options: {
-                compilerFile: 'lib/closure-compiler/compiler.jar',
+                compilerFile: 'bower_components/closure-compiler/compiler.jar',
                 compilerOpts: {
                     compilation_level: 'ADVANCED',
-                    externs: ['lib/closurecompiler-externs/*.js'],
-                    generate_exports: true,
-                    define: ["'goog.DEBUG=false'"],
-                    warning_level: 'verbose',
-                    summary_detail_level: 3,
-                    manage_closure_dependencies: true,
-                    only_closure_dependencies: true,
-                    closure_entry_point: [
-                        'javascript.util.ArrayList',
-                        'javascript.util.Arrays',
-                        'javascript.util.Collection',
-                        'javascript.util.EmptyStackException',
-                        'javascript.util.HashMap',
-                        'javascript.util.HashSet',
-                        'javascript.util.IndexOutOfBoundsException',
-                        'javascript.util.Iterator',
-                        'javascript.util.List',
-                        'javascript.util.Map',
-                        'javascript.util.NoSuchElementException',
-                        'javascript.util.OperationNotSupported',
-                        'javascript.util.Set',
-                        'javascript.util.SortedMap',
-                        'javascript.util.SortedSet',
-                        'javascript.util.Stack',
-                        'javascript.util.TreeMap',
-                        'javascript.util.TreeSet'
-                    ],
                     output_wrapper: '"(function(){%output%}).call(this);"'
                 },
                 execOpts: {
@@ -60,31 +29,14 @@ module.exports = function(grunt) {
                 TieredCompilation: true
             },
             browser: {
-                src: ['src/javascript/**/*.js',
-                    'lib/closure-library/closure/goog/**/*.js',
-                    '!lib/closure-library/closure/goog/**/*_test.js',
-                    '!lib/closure-library/closure/goog/**/*_perf.js',
-                    '!lib/closure-library/closure/goog/demos/**/*.js',
-                    'lib/closure-library/third_party/closure/goog/**/*.js',
-                    '!lib/closure-library/third_party/closure/goog/**/*_test.js',
-                    '!lib/closure-library/third_party/closure/goog/**/*_perf.js'
-                ],
+                src: ['src/javascript'],
                 dest: 'build/javascript.util.js'
             },
             node: {
                 TEMPcompilerOpts: {
                     only_closure_dependencies: false,
                 },
-                src: ['src/javascript/**/*.js',
-                    'lib/closure-library/closure/goog/**/*.js',
-                    '!lib/closure-library/closure/goog/**/*_test.js',
-                    '!lib/closure-library/closure/goog/**/*_perf.js',
-                    '!lib/closure-library/closure/goog/demos/**/*.js',
-                    'lib/closure-library/third_party/closure/goog/**/*.js',
-                    '!lib/closure-library/third_party/closure/goog/**/*_test.js',
-                    '!lib/closure-library/third_party/closure/goog/**/*_perf.js',
-                    'src/node.js'
-                ],
+                src: ['src/javascript', 'src/node.js' ],
                 dest: 'build/javascript.util-node.js'
             }
         },
@@ -106,7 +58,7 @@ module.exports = function(grunt) {
         closureDepsWriter: {
             main: {
                 options: {
-                    closureLibraryPath: 'lib/closure-library',
+                    closureLibraryPath: 'bower_components/closure-library',
                     root_with_prefix: '"src ../../../../src"',
                 },
                 dest: 'deps.js'
@@ -115,11 +67,10 @@ module.exports = function(grunt) {
         
     });
 
-    grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-closure-tools');
     grunt.loadNpmTasks('grunt-gjslint');
     grunt.loadNpmTasks('grunt-contrib-concat');
 
-    grunt.registerTask('default', ['bower', 'gjslint', 'closureDepsWriter']);
-    grunt.registerTask('dist', ['bower', 'gjslint', 'closureDepsWriter', 'closureCompiler', 'concat']);
+    grunt.registerTask('default', ['gjslint', 'closureDepsWriter']);
+    grunt.registerTask('dist', ['default', 'closureCompiler', 'concat']);
 }
